cd195c45828ac4b46f29bff3ac30f324a553f5cc
Martin Mittring <Kosmokleaner@Kosmokleaner.de>
2023-09-10 14:31:19 -0700

    started MT directory Iterator

--- MartinAssist.cpp
+++ MartinAssist.cpp
@@ -3,6 +3,47 @@
 #include "EveryHere.h"
 #include "Gui.h"
 
+// todo: move
+#include "Timer.h"
+struct DirectoryTraverseTest : public IDirectoryTraverse {
+
+    uint32 fileEntryCount = 0;
+    uint64 size = 0;
+    // from CTimer
+    double startTime = 0.0f;
+
+    DirectoryTraverseTest() {
+        startTime = g_Timer.GetAbsoluteTime();
+    }
+
+    virtual bool OnDirectory(const FilePath& filePath, const wchar_t* directory, const _wfinddata_t& findData) {
+        // to prevent compiler warning
+        filePath; directory; findData;
+        return true;
+    }
+
+    virtual void OnFile(const FilePath& path, const wchar_t* file, const _wfinddata_t& findData, float progress) 
+    {
+        // to prevent compiler warning
+        path; file; findData; progress;
+
+        ++fileEntryCount;
+    }
+};
+
+
+// depth first (memory efficient)
+// @param filePath e.g. L"c:\\temp" L"relative/Path" L""
+// @param pattern e.g. L"*.cpp" L"*.*", must not be 0
+void directoryTraverseMT(IDirectoryTraverse& sink, const FilePath& filePath, const wchar_t* pattern = L"*.*")
+{
+    // to prevent compiler warning
+    sink; filePath; pattern;
+
+}
+
+
+
 int main()
 {
 //    CppParserTest();
